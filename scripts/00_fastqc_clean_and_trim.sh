d07c1ec411ec44a1488da1590c71f467afbeb73b
Eldridge Wisely <31324469+EWisely@users.noreply.github.com>
2024-03-18 17:25:30 -0700

    Create 00_fastqc_clean_and_trim.sh

---
+++ scripts/00_fastqc_clean_and_trim.sh
@@ -0,0 +1,9 @@
+#Title: 00_fastqc_clean_and_trim.sh
+#Description: take demultiplexed reads and clean all primers, adapters, and low-quality bases out of them before metabarcoding analysis
+#Author: Eldridge Wisely
+#Date: March 18, 2024
+
+#Take arguments to specify primer set (have the ones I've already used pre-loaded or enter the primers and RC of them), and the adapter style (TruSeq of Nextera)
+
+
+#eDNA-Climb(2) primers F:  CGCGTCAAGAATGCCAGTCC  R: TCCAAACCCGGGGTGAACCT
